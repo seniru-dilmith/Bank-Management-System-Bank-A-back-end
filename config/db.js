6ec4f643c0be41aaa1921a57424fabe9b8fff4d7
Seniru Dilmith <dilmithseniru@gmail.com>
2024-09-20 00:59:03 +0530

    git ignore added

--- config/db.js
+++ config/db.js
@@ -1,10 +1,15 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
 
+dotenv.config();  // Load environment variables from a .env file into process.env
+
+// Create a connection pool
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'banking_system',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
-module.exports = pool;
+module.exports = pool.promise();  // Export the pool for use in our application
