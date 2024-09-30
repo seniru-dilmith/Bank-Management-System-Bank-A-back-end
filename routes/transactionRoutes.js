efa19926ef963113d7300b30f67cf0f62e56aa08
Sehan Dissanayake <sehan.22@cse.mrt.ac.lk>
2024-09-30 12:19:15 +0530

    reversed the changes made
    deleted the transaction and account summary code to write them again

--- routes/transactionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getRecentTransactions } = require('../controllers/transactionController');
-
-// Route to get recent transactions
-router.get('/transactions/recent', getRecentTransactions);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const dotenv = require('dotenv');
 const customerDetails = require('./routes/customerDetails');
 const appService = require('./services/appService')
-const transactionRoutes = require('./routes/transactionRoutes');
-const accountSummaryRoutes = require('./routes/accountSummaryRoutes');
 
 
 // Load environment variables from .env file
@@ -15,8 +13,6 @@ const app = express();
 app.use(express.json());  // Parse JSON request bodies
 app.use(customerDetails);  // Use the test-db route
 app.use(appService); // Use the appService route
-app.use(transactionRoutes);
-app.use(accountSummaryRoutes); 
 
 // Set up port from environment variables or default to 5000
 const PORT = process.env.PORT || process.env.BACKEND_PORT;
