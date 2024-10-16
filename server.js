a187e6d8f0f57965d803ec6f2c74053ecb45a0b9
Seniru Dilmith <dilmithseniru@gmail.com>
2024-10-16 17:03:46 +0530

    connected login page

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require('express');
 const dotenv = require('dotenv');
 const customerDetails = require('./routes/customerDetails');
 const appService = require('./services/appService')
-
+const cors = require('cors');
 
 // Load environment variables from .env file
 dotenv.config();
 
 // Initialize express app
 const app = express();
+
+// Enable CORS
+app.use(cors({
+    origin: 'http://localhost:3000',  // Allow requests from this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Specify allowed HTTP methods
+    credentials: true,  // If you are using cookies or sessions
+  }));
+  
 
 app.use(express.json());  // Parse JSON request bodies
 app.use(customerDetails);  // Use the test-db route
